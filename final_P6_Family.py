c0ac72e56d567be400f4be39bd6781903a61e3fd
dyzhangweix <dyzhangweix@gmail.com>
2014-08-11 11:01:01 -0700

    Create final_P6_Family.py

---
+++ final_P6_Family.py
@@ -0,0 +1,165 @@
+class Member(object):
+    def __init__(self, founder):
+        """ 
+        founder: string
+        Initializes a member. 
+        Name is the string of name of this node,
+        parent is None, and no children
+        """        
+        self.name = founder
+        self.parent = None         
+        self.children = []    
+
+    def __str__(self):
+        return self.name    
+
+    def add_parent(self, mother):
+        """
+        mother: Member
+        Sets the parent of this node to the `mother` Member node
+        """
+        self.parent = mother   
+
+    def get_parent(self):
+        """
+        Returns the parent Member node of this Member
+        """
+        return self.parent 
+
+    def is_parent(self, mother):
+        """
+        mother: Member
+        Returns: Boolean, whether or not `mother` is the 
+        parent of this Member
+        """
+        return self.parent == mother  
+
+    def add_child(self, child):
+        """
+        child: Member
+        Adds another child Member node to this Member
+        """
+        self.children.append(child)   
+
+    def is_child(self, child):
+        """
+        child: Member
+        Returns: Boolean, whether or not `child` is a
+        child of this Member
+        """
+        return child in self.children 
+
+
+
+class Family(object):
+    def __init__(self, founder):
+        """ 
+        Initialize with string of name of oldest ancestor
+
+        Keyword arguments:
+        founder -- string of name of oldest ancestor
+        """
+
+        self.names_to_nodes = {}
+        self.root = Member(founder)    
+        self.names_to_nodes[founder] = self.root   
+
+    def set_children(self, mother, list_of_children):
+        """
+        Set all children of the mother. 
+
+        Keyword arguments: 
+        mother -- mother's name as a string
+        list_of_children -- children names as strings
+        """
+        # convert name to Member node (should check for validity)
+        mom_node = self.names_to_nodes[mother]   
+        # add each child
+        for c in list_of_children:           
+            # create Member node for a child   
+            c_member = Member(c)               
+            # remember its name to node mapping
+            self.names_to_nodes[c] = c_member    
+            # set child's parent
+            c_member.add_parent(mom_node)        
+            # set the parent's child
+            mom_node.add_child(c_member)         
+    
+    def is_parent(self, mother, kid):
+        """
+        Returns True or False whether mother is parent of kid. 
+
+        Keyword arguments: 
+        mother -- string of mother's name
+        kid -- string of kid's name
+        """
+        mom_node = self.names_to_nodes[mother]
+        child_node = self.names_to_nodes[kid]
+        return child_node.is_parent(mom_node)   
+
+    def is_child(self, kid, mother):
+        """
+        Returns True or False whether kid is child of mother. 
+
+        Keyword arguments: 
+        kid -- string of kid's name
+        mother -- string of mother's name
+        """        
+        mom_node = self.names_to_nodes[mother]   
+        child_node = self.names_to_nodes[kid]
+        return mom_node.is_child(child_node)
+
+    def cousin(self, a, b):
+        """
+        Returns a tuple of (the cousin type, degree removed) 
+
+        Keyword arguments: 
+        a -- string that is the name of node a
+        b -- string that is the name of node b
+
+        cousin type:
+          -1 if a and b are the same node.
+          -1 if either one is a direct descendant of the other
+          >=0 otherwise, it calculates the distance from 
+          each node to the common ancestor.  Then cousin type is 
+          set to the smaller of the two distances, as described 
+          in the exercises above
+
+        degrees removed:
+          >= 0
+          The absolute value of the difference between the 
+          distance from each node to their common ancestor.
+        """
+        
+        if a==b: return (-1, 0)
+        a=self.names_to_nodes[a]
+        b=self.names_to_nodes[b]
+        if a.is_parent(b) or b.is_parent(a): return (-1, 1)
+        
+        current_a = a; current_b = b
+        visited=[]
+        while current_a != None or current_b != None:
+            if current_a in visited:
+                ancestor = current_a
+                break
+            elif current_a != None:
+                visited.append(current_a)
+                current_a = current_a.get_parent()
+
+            if current_b in visited:
+                ancestor = current_b
+                break
+            elif current_b != None:
+                visited.append(current_b)
+                current_b = current_b.get_parent()
+        cousinType = min(self.findDistance(ancestor, a), self.findDistance(ancestor, b))-1
+        degree = abs(self.findDistance(ancestor, a)-self.findDistance(ancestor, b))
+        return (cousinType, degree)
+    
+    
+    def findDistance(self, ancestor, a):
+        d=0
+        while a!=ancestor:
+            a=a.get_parent()
+            d+=1
+        return d
